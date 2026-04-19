6307c27257ce2b173a1624d1b5554de417c5b104
AlexJosalux <alexlumbi03@outlook.es>
2026-04-19 13:38:55 -0500

    Diseño

---
+++ src/app/features/libros-page/libros-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-libros-page',
+  imports: [],
+  templateUrl: './libros-page.html',
+  styleUrl: './libros-page.css',
+})
+export class LibrosPage {
+
+}
